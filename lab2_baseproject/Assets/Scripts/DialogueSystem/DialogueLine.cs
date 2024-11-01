184f8fc9a874e08a3c7c0a3b054210e51e8a12ac
Beamlak-h <beamlak@ualberta.ca>
2024-11-01 16:52:21 -0600

    Intro2 with dialogue box

---
+++ lab2_baseproject/Assets/Scripts/DialogueSystem/DialogueLine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+
+        //[Header ("Text Options")]
+        [SerializeField] private string input;
+        //[SerializeField] private Color textColor;
+        //[SerializeField] private Font textFont;
+
+        [Header("Time parameters")]
+        [SerializeField] private float delay;
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+            textHolder.text = "";
+
+            StartCoroutine(WriteText(input, textHolder, delay));
+
+        }
+        
+       
+    }
+}
+
--- lab2_baseproject/Assets/Scripts/IntroStory.cs
+++ lab2_baseproject/Assets/Scripts/IntroStory.cs
@@ -6,9 +6,19 @@ using UnityEngine.SceneManagement;
 public class IntroStory : MonoBehaviour
 {
     // Start is called before the first frame update
+    [Header("Scene Settings")]
+    [SerializeField] private string sceneName;
   
     void OnEnable()
     {
-        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.LogError("Scene name is not set!");
+        }
+        
     }
 }
