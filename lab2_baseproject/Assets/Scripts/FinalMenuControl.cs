b08ef8e22719d2f8e81f4a2ce72695dc65ef17cb
Beamlak-h <beamlak@ualberta.ca>
2024-11-09 09:52:24 -0700

    Stairs, End cutscene and Final Menu added

---
+++ lab2_baseproject/Assets/Scripts/FinalMenuControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalMenuControl : MonoBehaviour
+{
+    [SerializeField] private string nextSceneName;
+    public void LoadScene(string sceneName)
+    {
+        // Loads the scene with the given name
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        // Exits the application
+        Debug.Log("Quit Game"); 
+        Application.Quit();
+    }
+}
