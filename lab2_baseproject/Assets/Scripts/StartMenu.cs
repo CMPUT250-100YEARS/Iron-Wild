ea406d57d3e1b9d4593d8aad6bd22f4c4751e75d
Rheanna365 <rglass@ualberta.ca>
2024-11-10 17:37:12 -0700

    start menu functional

---
+++ lab2_baseproject/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button level1Button;
+    public Button level2Button;
+    public Button bossButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        level1Button.onClick.AddListener(() => LoadLevel("Intro_2"));
+        level2Button.onClick.AddListener(() => LoadLevel("Forest End"));
+        bossButton.onClick.AddListener(() => LoadLevel("StairsCutScene"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
