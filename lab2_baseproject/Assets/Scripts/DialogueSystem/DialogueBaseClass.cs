c2724489f1f6aa8f67d33a9d9bbff1474865d04c
Beamlak-h <beamlak@ualberta.ca>
2024-12-07 12:27:51 -0700

    Game name + Intro scene

--- lab2_baseproject/Assets/Scripts/DialogueSystem/DialogueBaseClass.cs
+++ lab2_baseproject/Assets/Scripts/DialogueSystem/DialogueBaseClass.cs
@@ -13,7 +13,7 @@ namespace DialogueSystem
         //[SerializeField] private CanvasGroup backgroundCanvasGroup; // Reference to CanvasGroup for fade effect
         //[SerializeField] private float fadeDuration = 1f; // Duration for fade in/out
 
-        protected IEnumerator WriteText (string input, Text textHolder, float delay, float delayBetweenLines )
+        protected IEnumerator WriteText (string input, Text textHolder, float baseDelay, float delayBetweenLines )
         {
 
             //***************
@@ -24,10 +24,19 @@ namespace DialogueSystem
             //textHolder.font = textFont;
             finished = false; // Reset finished status
             textHolder.text = ""; // Clear text before starting
+            float delay = baseDelay;
 
             for (int i = 0; i < input.Length; i++)
             {
                 textHolder.text += input[i];
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    delay = Mathf.Max(0.01f, delay - 0.01f);
+                }
+                else
+                {
+                    delay = baseDelay;
+                }
                 //play letter sound;
                 yield return new WaitForSeconds(delay);
             }
