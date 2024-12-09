66d9311e9ad65d2a8856602725a70e16ad281648
Rheanna365 <rglass@ualberta.ca>
2024-12-08 20:46:48 -0700

    credits & start menu buttons

--- lab2_baseproject/Assets/Scripts/StartMenu.cs
+++ lab2_baseproject/Assets/Scripts/StartMenu.cs
@@ -9,6 +9,7 @@ public class StartMenu : MonoBehaviour
     public Button level1Button;
     public Button level2Button;
     public Button bossButton;
+    public Button creditsButton;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class StartMenu : MonoBehaviour
         level1Button.onClick.AddListener(() => LoadLevel("Intro_2"));
         level2Button.onClick.AddListener(() => LoadLevel("City1"));
         bossButton.onClick.AddListener(() => LoadLevel("StairsCutScene"));
+        creditsButton.onClick.AddListener(() => LoadLevel("Credits"));
     }
 
     // Update is called once per frame
